8d61d9c8e21c5d79bf9222982faec31f4ec4d6a9
W. Hampson <thehambone93@gmail.com>
2018-03-28 17:34:08 -0500

    Struct size verification

---
+++ PigeonLocator/PigeonLocator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using WHampson.PigeonLocator.IvSave;
+
+namespace WHampson.PigeonLocator
+{
+    class PigeonLocator
+    {
+        static void PrintSizeOf(Type t)
+        {
+            Console.WriteLine("SizeOf({0}) = {1}", t.Name, Marshal.SizeOf(t));
+        }
+
+        static void Main(string[] args)
+        {
+            PrintSizeOf(typeof(FileHeader));
+            PrintSizeOf(typeof(BlockHeader));
+            PrintSizeOf(typeof(Pickup));
+            Console.ReadLine();
+        }
+    }
+}
