a9ba5a4182daa2e9dfa6a07057b5f704ae0357cb
W. Hampson <thehambone93@gmail.com>
2018-03-28 15:31:58 -0500

    Add assembly information

---
+++ PigeonLocator/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pigeon Locator")]
+[assembly: AssemblyDescription("Shows a map of remaining pigeons in a GTA IV save.")]
+[assembly: AssemblyProduct("Pigeon Locator")]
+[assembly: AssemblyCopyright("Copyright (C) 2018 W. Hampson")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("79650479-48f6-4af7-aa25-f64435d1c574")]
+
+
+[assembly: AssemblyVersion("0.1.0.3")]
+[assembly: AssemblyFileVersion("0.1.0.3")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha-03")]
