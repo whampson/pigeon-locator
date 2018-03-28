f08950fc76df40b0f72cc19cd14c5413eea59c8b
W. Hampson <thehambone93@gmail.com>
2018-03-28 17:48:01 -0500

    Stub-out IvSavegame class

---
+++ PigeonLocator/IvSavegame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WHampson.PigeonLocator
+{
+    internal class IvSavegame
+    {
+        public static IvSavegame Load(string path)
+        {
+            return null;
+        }
+
+        private IvSavegame()
+        {
+
+        }
+
+        public Vect3d[] GetRemainingPigeonLocations()
+        {
+            return new Vect3d[0];
+        }
+    }
+}
