e2f06e27cd5c666f7d02a41d060debd716796063
W. Hampson <thehambone93@gmail.com>
2018-03-28 19:59:10 -0500

    Add license header; test loading files

--- PigeonLocator/PigeonLocator.cs
+++ PigeonLocator/PigeonLocator.cs
@@ -1,24 +1,47 @@
-﻿using System;
+﻿#region License
+/* Copyright (c) 2018 W. Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using WHampson.PigeonLocator.IvSave;
+using System.Windows.Forms;
 
 namespace WHampson.PigeonLocator
 {
     class PigeonLocator
     {
-        static void PrintSizeOf(Type t)
-        {
-            Console.WriteLine("SizeOf({0}) = {1}", t.Name, Marshal.SizeOf(t));
-        }
-
         static void Main(string[] args)
         {
-            PrintSizeOf(typeof(FileHeader));
-            PrintSizeOf(typeof(BlockHeader));
-            PrintSizeOf(typeof(Pickup));
+            string testFile = Environment.GetEnvironmentVariable("LocalAppData")
+                + @"\Rockstar Games\GTA IV\savegames\user_1000100010001000\SGTA407";
+
+            string testFile2 = @"C:\Users\Wes\Documents\GTA3 User Files\GTA3SF1.b";
+
+            IvSavegame savegame = IvSavegame.Load(testFile);
+            Console.WriteLine(savegame.ToString());
+
             Console.ReadLine();
         }
     }
