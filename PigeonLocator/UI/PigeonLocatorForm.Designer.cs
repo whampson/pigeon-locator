f42af1955475930f7a6c9182eceb37926cc3434b
W. Hampson <thehambone93@gmail.com>
2018-03-31 16:23:51 -0500

    Move UI elements to 'UI' folder

---
+++ PigeonLocator/UI/PigeonLocatorForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿#region License
+/* Copyright (c) 2018 W. Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+namespace WHampson.PigeonLocator
+{
+    partial class PigeonLocatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileOpenMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileMenuSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.fileInformationMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileMenuSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.fileExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpAboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(918, 40);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileOpenMenuItem,
+            this.fileMenuSeparator1,
+            this.fileInformationMenuItem,
+            this.fileMenuSeparator2,
+            this.fileExitMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // fileOpenMenuItem
+            // 
+            this.fileOpenMenuItem.Name = "fileOpenMenuItem";
+            this.fileOpenMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.fileOpenMenuItem.Size = new System.Drawing.Size(341, 38);
+            this.fileOpenMenuItem.Text = "Open...";
+            this.fileOpenMenuItem.Click += new System.EventHandler(this.FileOpenMenuItem_OnClick);
+            // 
+            // fileMenuSeparator1
+            // 
+            this.fileMenuSeparator1.Name = "fileMenuSeparator1";
+            this.fileMenuSeparator1.Size = new System.Drawing.Size(338, 6);
+            // 
+            // fileInformationMenuItem
+            // 
+            this.fileInformationMenuItem.Name = "fileInformationMenuItem";
+            this.fileInformationMenuItem.Size = new System.Drawing.Size(341, 38);
+            this.fileInformationMenuItem.Text = "View File Information";
+            this.fileInformationMenuItem.Click += new System.EventHandler(this.FileInformationMenuItem_OnClick);
+            // 
+            // fileMenuSeparator2
+            // 
+            this.fileMenuSeparator2.Name = "fileMenuSeparator2";
+            this.fileMenuSeparator2.Size = new System.Drawing.Size(338, 6);
+            // 
+            // fileExitMenuItem
+            // 
+            this.fileExitMenuItem.Name = "fileExitMenuItem";
+            this.fileExitMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.fileExitMenuItem.Size = new System.Drawing.Size(341, 38);
+            this.fileExitMenuItem.Text = "Exit";
+            this.fileExitMenuItem.Click += new System.EventHandler(this.FileExitMenuItem_OnClick);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpAboutMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(77, 36);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // helpAboutMenuItem
+            // 
+            this.helpAboutMenuItem.Name = "helpAboutMenuItem";
+            this.helpAboutMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.helpAboutMenuItem.Size = new System.Drawing.Size(219, 38);
+            this.helpAboutMenuItem.Text = "About";
+            this.helpAboutMenuItem.Click += new System.EventHandler(this.HelpAboutMenuItem_OnClick);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 661);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(918, 38);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(55, 33);
+            this.statusLabel.Text = "Foo";
+            // 
+            // PigeonLocatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 699);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "PigeonLocatorForm";
+            this.Text = "GTA IV Pigeon Locator";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileOpenMenuItem;
+        private System.Windows.Forms.ToolStripSeparator fileMenuSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem fileInformationMenuItem;
+        private System.Windows.Forms.ToolStripSeparator fileMenuSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem fileExitMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpAboutMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
---
+++ PigeonLocator/UI/PigeonLocatorForm.cs
@@ -0,0 +1,140 @@
+﻿#region License
+/* Copyright (c) 2018 W. Hampson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WHampson.PigeonLocator.Properties;
+
+namespace WHampson.PigeonLocator
+{
+    public partial class PigeonLocatorForm : Form
+    {
+        private IvSavegame _savegame;
+
+        public PigeonLocatorForm()
+        {
+            InitializeComponent();
+            Savegame = null;
+            Status = "No file loaded.";
+        }
+
+        private IvSavegame Savegame
+        {
+            get { return _savegame; }
+            set {
+                _savegame = value;
+                fileInformationMenuItem.Enabled = (_savegame != null);
+            }
+        }
+
+        private string Status
+        {
+            get { return statusLabel.Text; }
+            set { statusLabel.Text = value; }
+        }
+
+        private void LoadFile(string path)
+        {
+            try {
+                Savegame = IvSavegame.Load(path);
+                Status = string.Format("Loaded '{0}'.", Savegame.LastMissionName);
+            } catch (FileNotFoundException ex) {
+                string title = "File Not Found";
+                string fmt = "The following file could not be found: {0}";
+                ShowErrorDialog(title, string.Format(fmt, path));
+            } catch (InvalidDataException ex) {
+                string title = "Invalid File Format";
+                string msg = "Not a valid GTA IV savedata file!";
+                ShowErrorDialog(title, msg);
+            }
+        }
+
+        private string GetGameUserDataDirectory()
+        {
+            return Environment.GetEnvironmentVariable("LocalAppData")
+                + @"\Rockstar Games\GTA IV";
+        }
+
+        private void ShowErrorDialog(string title, string msg)
+        {
+            MessageBox.Show(this, msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ShowInfoDialog(string title, string msg)
+        {
+            MessageBox.Show(this, msg, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void FileOpenMenuItem_OnClick(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ValidateNames = true;
+            fileDialog.Multiselect = false;
+            fileDialog.InitialDirectory = GetGameUserDataDirectory();
+
+            DialogResult result = fileDialog.ShowDialog();
+            if (result == DialogResult.OK) {
+                LoadFile(fileDialog.FileName);
+            }
+        }
+
+        private void FileInformationMenuItem_OnClick(object sender, EventArgs e)
+        {
+            if (Savegame == null) {
+                return;
+            }
+
+            string fileInfo = string.Format(
+                "Mission Name: {0}\n" +
+                "File Size: {1}\n" +
+                "File Version: {2}",
+                Savegame.LastMissionName,
+                Savegame.FileSize,
+                Savegame.FileVersion);
+
+            ShowInfoDialog("File Information", fileInfo);
+        }
+
+        private void FileExitMenuItem_OnClick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void HelpAboutMenuItem_OnClick(object sender, EventArgs e)
+        {
+            string aboutString = string.Format(
+                "{0}\nVersion: {1}\n\n{2}",
+                PigeonLocator.GetProgramName(),
+                PigeonLocator.GetProgramVersion(),
+                PigeonLocator.GetCopyright());
+            ShowInfoDialog("About", aboutString);
+        }
+    }
+}
