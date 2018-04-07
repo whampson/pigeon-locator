761fbc61bd0ead602b3be3c2c684f0e45ef4c5ca
W. Hampson <thehambone93@gmail.com>
2018-04-07 13:58:17 -0500

    Remove unnecessary 'using' directives

--- PigeonLocator/IniFile.cs
+++ PigeonLocator/IniFile.cs
@@ -21,10 +21,7 @@
  */
 #endregion
 
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
--- PigeonLocator/Program.cs
+++ PigeonLocator/Program.cs
@@ -25,7 +25,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace WHampson.PigeonLocator
