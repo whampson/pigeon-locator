957e9c6b22540f50422818d63e08e7bec690e21c
W. Hampson <thehambone93@gmail.com>
2018-04-07 13:57:23 -0500

    Documentation

--- PigeonLocator/IniFile.cs
+++ PigeonLocator/IniFile.cs
@@ -30,6 +30,9 @@ using System.Text;
 
 namespace WHampson.PigeonLocator
 {
+    /// <summary>
+    /// Represents a Windows .ini configuration file.
+    /// </summary>
     internal class IniFile
     {
         private const int MaxValueLength = 255;
@@ -42,13 +45,29 @@ namespace WHampson.PigeonLocator
         private static extern int WritePrivateProfileString(
             string sec, string key, string val, string path);
 
+        /// <summary>
+        /// Instantiates a new <see cref="IniFile"/> object.
+        /// </summary>
+        /// <param name="path">The path to the .ini file on disk.</param>
         public IniFile(string path)
         {
             FilePath = Path.GetFullPath(path);
         }
 
+        /// <summary>
+        /// Gets the path to the .ini file on disk.
+        /// </summary>
         public string FilePath { get; }
 
+        /// <summary>
+        /// Reads a value from the .ini file.
+        /// </summary>
+        /// <param name="key">The key to read.</param>
+        /// <param name="section">The section containing the key to read.</param>
+        /// <returns>
+        /// The value of the key read.
+        /// If the key doesn't exist, <see cref="String.Empty"/> is returned.
+        /// </returns>
         public string Read(string key, string section = null)
         {
             StringBuilder retval = new StringBuilder(MaxValueLength);
@@ -59,16 +78,42 @@ namespace WHampson.PigeonLocator
             return retval.ToString();
         }
 
+        /// <summary>
+        /// Writes a value to the .ini file.
+        /// </summary>
+        /// <param name="key">The key to write.</param>
+        /// <param name="value">The new value.</param>
+        /// <param name="section">The section containing the key to write.</param>
         public void Write(string key, string value, string section = null)
         {
             WritePrivateProfileString(section ?? Program.GetAssemblyName(), key, value, FilePath);
         }
 
+        /// <summary>
+        /// Checks whether a key-value pare exists in the .ini file.
+        /// </summary>
+        /// <param name="key">The key to check for existence.</param>
+        /// <param name="section">The section containing the key to check for.</param>
+        /// <returns>True if the key-value pair exists, False otherwise.</returns>
+        public bool ContainsKey(string key, string section = null)
+        {
+            return Read(key, section) != string.Empty;
+        }
+
+        /// <summary>
+        /// Deletes a key-value pair.
+        /// </summary>
+        /// <param name="key">The key to delete.</param>
+        /// <param name="section">The section containing the key to delete.</param>
         public void DeleteKey(string key, string section = null)
         {
             Write(key, null, section);
         }
 
+        /// <summary>
+        /// Deletes a section of key-value pairs.
+        /// </summary>
+        /// <param name="section">The section to delete.</param>
         public void DeleteSection(string section = null)
         {
             Write(null, null, section);
