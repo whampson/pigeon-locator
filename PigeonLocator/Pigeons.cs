25d7e10c15b96f820933f35fdcfcfaabaa61eb3d
W. Hampson <thehambone93@gmail.com>
2018-04-12 14:29:50 -0500

    Add descriptions for Dukes pigeons

--- PigeonLocator/Pigeons.cs
+++ PigeonLocator/Pigeons.cs
@@ -175,6 +175,198 @@ namespace WHampson.PigeonLocator
                     "On the metal railing at the building entrance\n" +
                     "by two semi trailers. Just east of your\n" +
                     "Bohan safehouse." },
+
+                // Bohan/Dukes/Broker 23
+                { new Vect3d(1414f, 1242f, 1.45f),
+                    "Dukes Bay Bridge\n" +
+                    "Underneath the bridge, on the support\n" +
+                    "platform." },
+
+                // Bohan/Dukes/Broker 24
+                { new Vect3d(909.86f, 941.18f, 11.86f),
+                    "Steinway\n" +
+                    "On the peal of a mausoleum along the\n" +
+                    "cemetery path." },
+
+                // Bohan/Dukes/Broker 25
+                { new Vect3d(1095.84f, 999.46f, 15.355f),
+                    "Steinway\n" +
+                    "Atop a play structure in the playground." },
+
+                // Bohan/Dukes/Broker 26
+                { new Vect3d(1252.98f, 994.53f, 15.047f),
+                    "Steinway\n" +
+                    "Atop the wall along the sidewalk, next to\n" +
+                    "Gantry Park parking lot." },
+
+                // Bohan/Dukes/Broker 27
+                { new Vect3d(1254.6f, 996.55f, 12.74f),
+                    "Steinway\n" +
+                    "On the sidewalk next to the wall that lines\n" +
+                    "the Gantry Park parking lot." },
+
+                // Bohan/Dukes/Broker 28
+                { new Vect3d(1118.95f, 914.51f, 32.21f),
+                    "Steinway\n" +
+                    "On a boulder underneath a bushy tree; it's\n" +
+                    "hard to see." },
+
+                // Bohan/Dukes/Broker 29
+                { new Vect3d(1271.25f, 895.93f, 30.775f),
+                    "Steinway\n" +
+                    "On a boulder underneath a ramp." },
+
+                // Bohan/Dukes/Broker 30
+                { new Vect3d(744.34f, 606.49f, 34.86f),
+                    "East Borough Bridge\n" +
+                    "On the railing of the bridge's south side\n" +
+                    "walkway." },
+
+                // Bohan/Dukes/Broker 31
+                { new Vect3d(860f, 587f, 9.195f),
+                    "East Island City\n" +
+                    "On the ground in the pedestrian underpass\n" +
+                    "below Franklin St." },
+
+                // Bohan/Dukes/Broker 32
+                { new Vect3d(893.5f, 702f, 18.972f),
+                    "Steinway\n" +
+                    "On the highest tier of the pool's diving\n" +
+                    "platform." },
+
+                // Bohan/Dukes/Broker 33
+                { new Vect3d(1169.19f, 777.56f, 37.79f),
+                    "Steinway\n" +
+                    "In the tree across the street from Blarney's\n" +
+                    "pub." },
+
+                // Bohan/Dukes/Broker 34
+                { new Vect3d(1148.74f, 686.82f, 39.8f),
+                    "Steinway\n" +
+                    "On the top corner of the wooden fence in a\n" +
+                    "back alley." },
+
+                // Bohan/Dukes/Broker 35
+                { new Vect3d(1270.12f, 772.34f, 51.673f),
+                    "East Island City\n" +
+                    "On the rafter under the train station\n" +
+                    "platform's roof." },
+
+                // Bohan/Dukes/Broker 36
+                { new Vect3d(1320f, 666f, 50.55f),
+                    "East Island City\n" +
+                    "On the corner of the NUCA Design Store\n" +
+                    "(with NUCA banners hanging outside)\n" +
+                    "building's rooftop. It's inside the roof\n" +
+                    "enclosure, on the floor. Throw a Molotov or\n" +
+                    "grenade up from the street, or drop onto\n" +
+                    "the roof via helicopter." },
+
+                // Bohan/Dukes/Broker 37
+                { new Vect3d(1609f, 852f, 14.85f),
+                    "Meadows Park\n" +
+                    "At the abandoned circus big top that's\n" +
+                    "missing a tent. It's inside a doorway." },
+
+                // Bohan/Dukes/Broker 38
+                { new Vect3d(1489.13f, 614.275f, 29.553f),
+                    "Meadows Park\n" +
+                    "On the plaque at the base of the \"Goosed\"\n" +
+                    "statue." },
+
+                // Bohan/Dukes/Broker 39
+                { new Vect3d(1886.12f, 781.04f, 22.33f),
+                    "Francis International Airport\n" +
+                    "In the middle of a wooded area, on the\n" +
+                    "small pump utility house's top edge." },
+
+                // Bohan/Dukes/Broker 40
+                { new Vect3d(1748.98f, 651.22f, 33.475f),
+                    "Willis\n" +
+                    "On top of an apartment building's green\n" +
+                    "entrance awning." },
+
+                // Bohan/Dukes/Broker 41
+                { new Vect3d(1619.84f, 443.09f, 43.247f),
+                    "Meadow Hills\n" +
+                    "On the rooftop across the street from\n" +
+                    "the train station, in a recessed roof door\n" +
+                    "entry. From the train station platform, use\n" +
+                    "the bench to jump and climb onto the\n" +
+                    "westernmost structure's roof. Then follow\n" +
+                    "the roof across the street to the opposite roof." },
+
+                // Bohan/Dukes/Broker 42
+                { new Vect3d(1823.34f, 397.18f, 32.03f),
+                    "Willis\n" +
+                    "On the raised train tracks' cross-strut,\n" +
+                    "near the Car Wash & Lube." },
+
+                // Bohan/Dukes/Broker 43
+                { new Vect3d(2303.47f, 616.15f, 16.02f),
+                    "Francis International Airport\n" +
+                    "Tucked between two air conditioning units\n" +
+                    "on the airport terminal roof. It's in the flat\n" +
+                    "area between the two curved rooftops.\n" +
+                    "From the airport parking lot, climb the stairs\n" +
+                    "up to the train station platform. Follow the\n" +
+                    "tracks north until they overlap the terminal\n" +
+                    "roof. Jump down onto the roof and follow it\n" +
+                    "to the air conditioning units." },
+
+                // Bohan/Dukes/Broker 44
+                { new Vect3d(2367.38f, 368f, 10.68f),
+                    "Francis International Airport\n" +
+                    "In the terminal support strut's circular hole.\n" +
+                    "From the airport parking lot, climb the stairs\n" +
+                    "up to the train station platform. Follow the\n" +
+                    "tracks south until you can get a clear shot\n" +
+                    "with the sniper rifle." },
+
+                // Bohan/Dukes/Broker 45
+                { new Vect3d(2317.83f, 336.87f, 6.49f),
+                    "Francis International Airport\n" +
+                    "On the low post just under the airport parking\n" +
+                    "lot's main entrance gate, across from the pay\n" +
+                    "booth." },
+
+                // Bohan/Dukes/Broker 46
+                { new Vect3d(2618.475f, 416f, 79.835f),
+                    "Francis International Airport\n" +
+                    "On the balcony railing outside the airport\n" +
+                    "control tower's topmost level. Target it with\n" +
+                    "a sniper rifle near the opening to the runway\n" +
+                    "tarmac." },
+
+                // Bohan/Dukes/Broker 47
+                { new Vect3d(948.05f, 416.73f, 17.085f),
+                    "BOABO\n" +
+                    "Behind the Creek St Diner and next to\n" +
+                    "the big \"Storage\" sign, about 10 feet\n" +
+                    "up a freeway retaining wall." },
+
+                // Bohan/Dukes/Broker 48
+                { new Vect3d(1175.78f, 439.37f, 32.385f),
+                    "Cerveza Heights\n" +
+                    "Atop the basketball backboard in the back-\n" +
+                    "alley court." },
+
+                // Bohan/Dukes/Broker 49
+                { new Vect3d(1382.54f, 532f, 44.908f),
+                    "Meadows Park\n" +
+                    "Atop the row house's chimney on\n" +
+                    "Savannah Ave, at the end of the same block\n" +
+                    "as Ma McReary's place. Enter the door and\n" +
+                    "climb the stairs to the rooftop. Then climb\n" +
+                    "a bit to get a clear shot." },
+
+                // Bohan/Dukes/Broker 50
+                { new Vect3d(1411f, 405.42f, 35.12f),
+                    "Cerveza Heights\n" +
+                    "On the roof over the stairs that lead up to\n" +
+                    "the Huntington St train station's first level,\n" +
+                    "directly behind the bank of three ticket-\n" +
+                    "dispensing machines." },
             };
     }
 }
