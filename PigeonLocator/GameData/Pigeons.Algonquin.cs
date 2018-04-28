685d54a2125141d1f6d4c67884c952acbbb360a4
W. Hampson <thehambone93@gmail.com>
2018-04-28 17:16:10 -0500

    Finish Algonquin pigeons

--- PigeonLocator/GameData/Pigeons.Algonquin.cs
+++ PigeonLocator/GameData/Pigeons.Algonquin.cs
@@ -252,7 +252,9 @@ namespace WHampson.PigeonLocator.GameData
                     "Behind the girder on an old, abandoned\n" +
                     "raised rail line. This is across from Golden\n" +
                     "Pier and behind the Auto Limbo Pay 'n' Spray\n" +
-                    "that has a working spray can on top." },
+                    "that has a working spray can on top. Enter the\n" +
+                    "elevated railway from the stairs on Garnet St\n" +
+                    "between Frankfort Ave and Galveston Ave." },
 
                 // Algonquin 35
                 { new Vect3d(-287.41f, 236.85f, 204.392f),
@@ -261,6 +263,239 @@ namespace WHampson.PigeonLocator.GameData
                     "The flying rat is on the tier below the helipad.\n" +
                     "Use a helicopter or the window cleaning lift to\n" +
                     "access the rooftop." },
+
+                // Algonquin 36
+                { new Vect3d(-247.77f, 243.06f, 16.195f),
+                    "Star Junction\n" +
+                    "On the sidewalk power box against the\n" +
+                    "MeTV building's north side (below the giant\n" +
+                    "turntable)." },
+
+                // Algonquin 37
+                { new Vect3d(-180.08f, 210f, 17.49f),
+                    "Star Junction\n" +
+                    "In a sidewalk tree on the west side of the\n" +
+                    "BAWSAQ building." },
+
+                // Algonquin 38
+                { new Vect3d(41f, 109.88f, 15.015f),
+                    "Easton\n" +
+                    "Outside a police station, behind the statue\n" +
+                    "of Neptune." },
+
+                // Algonquin 39
+                { new Vect3d(156f, 226.32f, 21.025f),
+                    "Lancet\n" +
+                    "On the gondola platform on a rail between\n" +
+                    "the gondola entry points." },
+
+                // Algonquin 40
+                { new Vect3d(202f, 266f, 7.43f),
+                    "Lancet\n" +
+                    "Under the bridge, in the small crevasse\n" +
+                    "between the west side onramp and the\n" +
+                    "tunnel wall. Jump over the onramp rail and\n" +
+                    "follow the narrow path to the Pigeon." },
+
+                // Algonquin 41
+                { new Vect3d(428.09f, 238.91f, 14.7f),
+                    "Colony Island\n" +
+                    "On the gondola platform on a rail." },
+
+                // Algonquin 42
+                { new Vect3d(505f, 220.1f, 30.1f),
+                    "Colony Island\n" +
+                    "Atop an industrial building's north edge,\n" +
+                    "between smokestacks. You can reach this\n" +
+                    "rooftop from the rooftop of the adjacent\n" +
+                    "building to the north. Use the street-level\n" +
+                    "fire escape to reach the roof. Jump down to\n" +
+                    "the fallen billboard bridge to the target\n" +
+                    "building, then use the stairs to reach the\n" +
+                    "Pigeon." },
+
+                // Algonquin 43
+                { new Vect3d(378.33f, 123.22f, 5.757f),
+                    "Colony Island\n" +
+                    "On the steps near the river's edge walkway." },
+
+                // Algonquin 44
+                { new Vect3d(483.71f, 100.54f, 7.738f),
+                    "Colony Island\n" +
+                    "On the broken merry-go-round in the small\n" +
+                    "park between apartment buildings." },
+
+                // Algonquin 45
+                { new Vect3d(270.34f, 31.24f, 4.325f),
+                    "President's City\n" +
+                    "Under the freeway in a pile of trash." },
+
+                // Algonquin 46
+                { new Vect3d(510.72f, -51.88f, 15.958f),
+                    "Colony Island\n" +
+                    "At the abandoned mental hospital, in the\n" +
+                    "east side entrance's top windowsill." },
+
+                // Algonquin 47
+                { new Vect3d(-463.6f, 7.32f, 11.42f),
+                    "The Meat Quarter\n" +
+                    "On top of the bus stop shelter that's across\n" +
+                    "the street (west) from the Ron Oil gas\n" +
+                    "station." },
+
+                // Algonquin 48
+                { new Vect3d(-407.96f, -84.36f, 14.304f),
+                    "The Meat Quarter\n" +
+                    "Under the beginning section of the\n" +
+                    "abandoned, raised rail line. Use the stairs\n" +
+                    "on Garnet St between Frankfort Ave and\n" +
+                    "Galveston Ave to reach the tracks. Then,\n" +
+                    "drop through the crevasse between the road\n" +
+                    "barricades and the tracks." },
+
+                // Algonquin 49
+                { new Vect3d(-298f, -84.76f, 335.23f),
+                    "The Triangle\n" +
+                    "On the Rotterdam Tower's top pedestrian\n" +
+                    "lookout ledge, on the north side. Enter\n" +
+                    "the building through the marker on\n" +
+                    "Denver-Exeter Ave." },
+
+                // Algonquin 50
+                { new Vect3d(-124.08f, 15.92f, 31.895f),
+                    "The Triangle\n" +
+                    "Inside the marquee Shark's mouth on the\n" +
+                    "corner of Iron St and Burlesque. Snipe it\n" +
+                    "from the kitty-corner sidewalk." },
+
+                // Algonquin 51
+                { new Vect3d(-68.19f, -91.27f, 18.398f),
+                    "The Triangle\n" +
+                    "The Viendemonte restaurant next to Al\n" +
+                    "Dente and across the street from Fanny\n" +
+                    "Crabs. It's on the sidewalk covering the\n" +
+                    "top corner." },
+
+                // Algonquin 52
+                { new Vect3d(240f, -172f, 3.98f),
+                    "Fishmarket North\n" +
+                    "In a little alcove under the freeway. Use the\n" +
+                    "sidewalk steps to reach the alcove, then climb\n" +
+                    "over the rickety fence to shoot the bird." },
+
+                // Algonquin 53
+                { new Vect3d(-454.6f, -255.5f, 6.91f),
+                    "Suffolk\n" +
+                    "In a small, wooded area between streets,\n" +
+                    "hidden by a tall, rickety fence. It's on the\n" +
+                    "ground under one of the trees." },
+
+                // Algonquin 54
+                { new Vect3d(-319.983f, -291.555f, 13.75f),
+                    "Suffolk\n" +
+                    "On a gravestone behind the church." },
+
+                // Algonquin 55
+                { new Vect3d(-284.57f, -391.71f, 9f),
+                    "City Hall\n" +
+                    "On a railing in the alley with a Cluckin' Bell\n" +
+                    "billboard." },
+
+                // Algonquin 56
+                { new Vect3d(-85.66f, -341.33f, 14.87f),
+                    "City Hall\n" +
+                    "Perched on a low column of a building. It's\n" +
+                    "on the north courtyard side." },
+
+                // Algonquin 57
+                { new Vect3d(0241.22f, -417.16f, 8.015f),
+                    "Fishmarket South\n" +
+                    "At the skate park under the Broker Bridge.\n" +
+                    "It's on the top edge of a skateboard ramp." },
+
+                // Algonquin 58
+                { new Vect3d(493.85f, -389.98f, 85.427f),
+                    "Broker Bridge\n" +
+                    "On top of the Broker Bridge's west structural\n" +
+                    "column. Use a helicopter to reach this area." },
+
+                // Algonquin 59
+                { new Vect3d(337.66f, -640.09f, 4.528f),
+                    "Fishmarket South\n" +
+                    "Perched on a mooring at the edge of the\n" +
+                    "river near Higgins Helitours." },
+
+                // Algonquin 60
+                { new Vect3d(28.48f, -599.05f, 14.568f),
+                    "The Exchange\n" +
+                    "On a waist-high column in the alleyway\n" +
+                    "beside Bank of Liberty." },
+
+                // Algonquin 61
+                { new Vect3d(105.61f, -759.86f, 3.945f),
+                    "The Exchange\n" +
+                    "On the ground, smack-dab in the middle\n" +
+                    "of a narrow park. Look for the circular\n" +
+                    "center walkway." },
+
+                // Algonquin 62
+                { new Vect3d(-117.27f, -706.5f, 10.7f),
+                    "The Exchange\n" +
+                    "In a planter beside the southern twin\n" +
+                    "attached towers on Denver Ave." },
+
+                // Algonquin 63
+                { new Vect3d(-362.27f, -676.65f, 2.252f),
+                    "Castle Garden City\n" +
+                    "At the dicks on a boat ramp's rail." },
+
+                // Algonquin 64
+                { new Vect3d(114f, -962f, 4.18f),
+                    "Castle Gardens\n" +
+                    "In a large nook in the side of the tall\n" +
+                    "bulkhead. Snipe it from the water while\n" +
+                    "you stand on a boat." },
+
+                // Algonquin 65
+                { new Vect3d(-27.92f, -954f, 12.13f),
+                    "Castle Gardens\n" +
+                    "On top of a shipping container at the\n" +
+                    "docks. Climb up to the top of the adjacent\n" +
+                    "containers and snipe the bird." },
+
+                // Algonquin 66
+                { new Vect3d(-607.94f, -739.68f, 20.756f),
+                    "Happiness Island\n" +
+                    "On the second tier up from the statue's\n" +
+                    "star-shaped foundation level. It's on the\n" +
+                    "north side rail." },
+
+                // Algonquin 67
+                { new Vect3d(-616.71f, -752.78f, 72.82f),
+                    "Happiness Island\n" +
+                    "In the statue's left hand under the keystone." },
+
+                // Algonquin 68
+                { new Vect3d(-611.62f, -753.64f, 78.358f),
+                    "Happiness Island\n" +
+                    "On the statue's left shoulder." },
+
+                // Algonquin 69
+                { new Vect3d(-608.92f, -751.21f, 83.75f),
+                    "Happiness Island\n" +
+                    "On the statue's forehead, just below the\n" +
+                    "crown." },
+
+                // Algonquin 70
+                { new Vect3d(-606.24f, -748.39f, 91.15f),
+                    "Happiness Island\n" +
+                    "On the statue's right thumb (the hand\n" +
+                    "holding the cup of coffee)." },
+
+                // Algonquin 71
+                { new Vect3d(-606.395f, -748.9f, 92.46f),
+                    "Happiness Island\n" +
+                    "On top of the coffee cup in the right hand." },
             };
     }
 }
