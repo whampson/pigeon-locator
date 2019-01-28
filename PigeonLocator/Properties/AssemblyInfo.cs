b8e1aee919df08c34a831b2afa846a2eb1b2c0f2
W. Hampson <thehambone93@gmail.com>
2019-01-27 23:02:44 -0600

    Version 1.1.0 build 787

--- PigeonLocator/Properties/AssemblyInfo.cs
+++ PigeonLocator/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("79650479-48f6-4af7-aa25-f64435d1c574")]
 
 
-[assembly: AssemblyVersion("1.1.0.771")]
-[assembly: AssemblyFileVersion("1.1.0.771")]
+[assembly: AssemblyVersion("1.1.0.787")]
+[assembly: AssemblyFileVersion("1.1.0.787")]
 [assembly: AssemblyInformationalVersion("1.1.0")]
